78425ba0601727d7d2eb2464cb53aed47de4d769
Shuntaro Nishizawa <me@shun.technology>
2022-04-13 11:08:56 +0900

    feat(neovim): Add basic config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,175 @@
+-- autocmdの設定
+vim.cmd("augroup init")
+vim.cmd("autocmd!")
+vim.cmd("augroup END")
+
+-- キャッシュディレクトリの設定
+cache_dir = vim.env.XDG_CACHE_HOME .. "/nvim"
+
+-- バッファやレジスタ内で使用する文字コードを指定
+vim.o.encoding = "utf-8"
+
+-- 既存ファイルを開くときにVimが使用する文字コードを判定する順番を指定
+vim.o.fileencodings = "utf-8,iso-2033-jp,enc-jp,sjis"
+
+-- viminfoファイルの出力先を変更
+vim.o.viminfo = vim.o.viminfo .. ",n" .. cache_dir .. "/viminfo"
+
+-- Undo履歴を永続化
+vim.o.undodir = cache_dir .. "/nvimundo"
+vim.bo.undofile = true
+
+-- Swapファイルの出力先を変更
+vim.o.wildmenu = true
+-- 補完の仕方を始めに最長共通文字列を補完し、次に完全に補完
+vim.o.wildmode = "longest,full"
+
+-- 行番号を表示
+vim.wo.number = true
+
+-- スクロールにオフセットを持たせる
+vim.wo.scrolloff = 5
+
+-- 自動改行を無効化
+vim.bo.textwidth = 0
+
+-- 不可視文字を表示
+vim.wo.list = true
+-- 不可視文字のスタイルを変更
+vim.o.listchars = "tab:»-,trail:-,extends:»,precedes:«,nbsp:%,eol:↵"
+
+-- カーソルラインを表示
+vim.wo.cursorline = true
+
+-- タブラインを常に表示
+vim.o.showtabline = 2
+
+-- 検索結果を自動ハイライト
+vim.o.hlsearch = true
+
+-- 検索時のみ大文字小文字を区別しない
+vim.cmd("autocmd init CmdlineEnter [/?] set ignorecase")
+vim.cmd("autocmd init CmdlineLeave [/?] set noignorecase")
+
+-- 大文字が含まれる場合は区別する
+vim.o.smartcase = true
+
+-- インクリメンタルサーチを有効化
+vim.o.incsearch = true
+
+-- バックスラッシュやエクステンションを自動エスケープ
+vim.api.nvim_set_keymap(
+  "c",
+  "/",
+  "getcmdtype() == '/' ? '\\/' : '/'",
+  { noremap = true, expr = true }
+)
+vim.api.nvim_set_keymap(
+  "c",
+  "?",
+  "getcmdtype() == '?' ? '\\?' : '?'",
+  { noremap = true, expr = true }
+)
+
+-- 手動インデント時に"shiftwidth"の倍数に丸める
+vim.o.shiftround = true
+
+-- 矩形選択時にテキストが存在しない部分でも選択できるようにする
+vim.o.virtualedit = "block"
+
+-- バッファを閉じずに隠す
+vim.o.hidden = true
+
+-- ファイルを新しく開く代わりにすでに開いてあるバッファを使用する
+vim.o.switchbuf = "useopen"
+
+-- 対応する括弧をハイライト表示する
+vim.o.showmatch = true
+-- ハイライト表示の時間を設定
+vim.o.matchtime = 2
+
+-- バックスラッシュで何でも消せるようにする
+vim.o.backspace = "indent,eol,start"
+
+-- タブをスペースに展開
+vim.bo.expandtab = true
+
+-- インデントを考慮して改行する
+vim.bo.smartindent = true
+
+-- 補完の仕方を変更
+vim.o.completeopt = "menuone,noinsert,noselect"
+
+-- 補完時にメッセージを出さない
+vim.o.shortmess = vim.o.shortmess .. "c"
+
+-- 挿入モード中にjjで<Esc>
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true })
+
+-- [Prefix]というマッピングを追加
+vim.api.nvim_set_keymap("n", "[Prefix]", "", { noremap = true })
+-- <Space>に[Prefix]を割り当て
+vim.api.nvim_set_keymap("n", "<Space>", "[Prefix]", {})
+
+-- <Esc>x2でハイライトオフ
+vim.api.nvim_set_keymap(
+  "n",
+  "<Esc><Esc>",
+  "<Cmd>nohlsearch<CR>",
+  { noremap = true, silent = true }
+)
+
+-- 検索結果を画面中央に表示
+vim.api.nvim_set_keymap("n", "n", "nzz", { noremap = true })
+vim.api.nvim_set_keymap("n", "N", "Nzz", { noremap = true })
+vim.api.nvim_set_keymap("n", "*", "*zz", { noremap = true })
+vim.api.nvim_set_keymap("n", "#", "#zz", { noremap = true })
+vim.api.nvim_set_keymap("n", "g*", "g*zz", { noremap = true })
+vim.api.nvim_set_keymap("n", "g#", "g#zz", { noremap = true })
+
+-- 表示行でj, k移動
+vim.api.nvim_set_keymap("n", "j", "gj", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "k", "gk", { noremap = true, silent = true })
+
+-- vx2で行末まで選択
+vim.api.nvim_set_keymap("v", "v", "$h", { noremap = true })
+
+-- Ctrl + hjklでウィンドウ間移動
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true })
+-- Ctrl + 矢印キーでウィンドウリサイズ
+vim.api.nvim_set_keymap("n", "<S-Left>",  "<C-w><", { noremap = true })
+vim.api.nvim_set_keymap("n", "<S-Down>",  "<C-w>+", { noremap = true })
+vim.api.nvim_set_keymap("n", "<S-Up>",    "<C-w>-", { noremap = true })
+vim.api.nvim_set_keymap("n", "<S-Right>", "<C-w>>", { noremap = true })
+
+-- QuickFix及びHelpではqで閉じる
+vim.cmd("autocmd init FileType help,qf nnoremap <buffer> q <C-w>c")
+
+-- w!!でスーパーユーザーとして保存
+vim.api.nvim_set_keymap("c", "w!!", "w !sudo tee > /dev/null %", {})
+
+-- H, Lで行頭、行末へ移動
+vim.api.nvim_set_keymap("n", "H", "^", { noremap = true })
+vim.api.nvim_set_keymap("n", "L", "$", { noremap = true })
+
+-- [Prefix] + tでターミナルを開く
+vim.api.nvim_set_keymap(
+  "n",
+  "[Prefix]t",
+  "<Cmd>botright 20split +term<CR>i",
+  { noremap = true, silent = true }
+)
+
+-- make, grepなどのコマンドの後に自動的にQuickFixを開く
+vim.cmd("autocmd init QuickfixCmdPost make,grep,grepadd,vimgrep copen")
+
+-- フォルダが存在しない場合に自動作成する
+function mkdir(dir, force)
+  if vim.fn.isdirectory(dir) and (force or string.match(vim.fn.input(vim.fn.printf('"%s" does not exist. Create? [y/N]', dir)), "^y(es)?$")) then
+    vim.fn.mkdir(dir, "p")
+  end
+end
+vim.cmd("autocmd init BufWritePre * call v:lua.mkdir(expand('<afile>:p:h'), v:cmdbang)")
