a417d410a513e5a513c3da62c0c8e4fa96871719
Shuntaro Nishizawa <me@shun.technology>
2022-04-17 01:36:36 +0900

    feat(neovim): Adjust theme

--- .config/nvim/lua/plugin/nord.lua
+++ .config/nvim/lua/plugin/nord.lua
@@ -1,6 +1,12 @@
 -- 背景を透過させる
 vim.g.nord_disable_background = true
+-- カーソルラインを透過させる
+vim.g.nord_cursorline_transparent = true
 -- ウィンドウの区切り線を表示
 vim.g.nord_borders = true
 
 require("nord").set()
+
+local colors = require("nord.colors")
+-- 不可視文字やバーチャルテキストをもう少し見やすくする
+vim.cmd("highlight NonText guifg=" .. colors.nord3_gui)
